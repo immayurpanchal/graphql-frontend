73063557afcb255f767ba7b70ba9877b05e5cb83
Mayur Panchal <mayur.panchal@deepintent.com>
2024-10-16 22:44:29 +0530

    feat: :sparkles: implement add book mutation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql, useMutation, useQuery } from '@apollo/client'
 import './App.scss'
 import { useState } from 'react'
 
@@ -14,6 +14,16 @@ const GET_BOOKS = gql`
 		getBooks(sort: $sort) {
 			name
 			id
+			authorId
+		}
+	}
+`
+
+const ADD_BOOK = gql`
+	mutation AddBook($name: String!, $authorId: Int!) {
+		addBook(name: $name, authorId: $authorId) {
+			name
+			id
 		}
 	}
 `
@@ -21,6 +31,7 @@ const GET_BOOKS = gql`
 type Book = {
 	name: string
 	id: string
+	authorId: number
 }
 
 type BooksData = {
@@ -35,8 +46,15 @@ function App() {
 		}
 	})
 
+	const [authorId, setAuthorId] = useState<number>(-1)
+	const [bookName, setBookName] = useState<string>('')
+
 	const { loading, error, data } = useQuery<BooksData, { sort: SortCriteria }>(GET_BOOKS, {
 		variables: { sort: { ...sortCriteria } }
+	})
+
+	const [addBookMutation, { loading: addLoading, error: addError }] = useMutation(ADD_BOOK, {
+		refetchQueries: [{ query: GET_BOOKS, variables: { sort: sortCriteria } }]
 	})
 
 	const onToggleSort = () => {
@@ -48,6 +66,10 @@ function App() {
 		})
 	}
 
+	const onAddBook = () => {
+		addBookMutation({ variables: { name: bookName, authorId } })
+	}
+
 	console.log(loading, error, data)
 
 	if (loading) return <>Loading</>
@@ -57,12 +79,18 @@ function App() {
 
 	return (
 		<>
+			{addLoading && !addError && <div>Adding Book Data. Please wait...</div>}
 			<button onClick={onToggleSort}>Toggle Sort</button>
 			{data.getBooks.map(book => (
 				<div key={book.id}>
-					{book.id} - {book.name}
+					{book.id} - {book.name} - {book.authorId}
 				</div>
 			))}
+			<div>
+				<input type="text" placeholder="Enter Author Id" onChange={e => setAuthorId(Number(e.target.value))} />
+				<input type="text" placeholder="Enter Book Name" onChange={e => setBookName(e.target.value)} />
+				<button onClick={onAddBook}>Add Book</button>
+			</div>
 		</>
 	)
 }
