4bc16a38777d32c0df63bf358f29546cbca72a11
Mayur Panchal <mayur.panchal@deepintent.com>
2024-10-16 09:41:34 +0530

    feat: :sparkles: implement graphql using @apollo/client

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,70 @@
+import { gql, useQuery } from '@apollo/client'
+import './App.scss'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+type SortCriteria = {
+	sortCriteria: {
+		order: 'ASC' | 'DESC'
+		field: string
+	}
+}
+
+const GET_BOOKS = gql`
+	query getMyBooks($sort: SortInput) {
+		getBooks(sort: $sort) {
+			name
+			id
+		}
+	}
+`
+
+type Book = {
+	name: string
+	id: string
+}
+
+type BooksData = {
+	getBooks: Book[]
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [sortCriteria, setSortCriteria] = useState<SortCriteria>({
+		sortCriteria: {
+			order: 'ASC',
+			field: 'id'
+		}
+	})
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const { loading, error, data } = useQuery<BooksData, { sort: SortCriteria }>(GET_BOOKS, {
+		variables: { sort: { ...sortCriteria } }
+	})
+
+	const onToggleSort = () => {
+		setSortCriteria({
+			sortCriteria: {
+				...sortCriteria.sortCriteria,
+				order: sortCriteria.sortCriteria.order === 'ASC' ? 'DESC' : 'ASC'
+			}
+		})
+	}
+
+	console.log(loading, error, data)
+
+	if (loading) return <>Loading</>
+	if (error) return <>Error</>
+
+	if (!data) return <>No data</>
+
+	return (
+		<>
+			<button onClick={onToggleSort}>Toggle Sort</button>
+			{data.getBooks.map(book => (
+				<div key={book.id}>
+					{book.id} - {book.name}
+				</div>
+			))}
+		</>
+	)
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import App from './App.tsx'
-import './index.css'
+import './index.scss'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const client = new ApolloClient({
+	uri: 'http://localhost:3000/graphql',
+	cache: new InMemoryCache()
+})
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+	<StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</StrictMode>
 )
